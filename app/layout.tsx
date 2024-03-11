0d6e8f2ae6ac9653fa6e0bc23b4fee3dd1f25b23
irishcarbomb777 <danielclannan@gmail.com>
2024-03-11 09:00:43 -0400

    Add GTM for Cam

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { Container, Theme } from '@radix-ui/themes';
+import { Theme } from '@radix-ui/themes';
 import '@radix-ui/themes/styles.css';
 import './globals.css'
 import { Header } from './components/Header';
@@ -47,6 +47,15 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en" suppressHydrationWarning className="scroll-smooth">
+            {process.env.NODE_ENV === "production" && 
+            <Script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-MLS2GQL2');
+            `}
+            </Script>
+            }
             {process.env.NODE_ENV === "production" && <Script>{`window[(function(_RiZ,_Ax){var _mVhei='';for(var _psi4sq=0;_psi4sq<_RiZ.length;_psi4sq++){var _VZwA=_RiZ[_psi4sq].charCodeAt();_VZwA-=_Ax;_Ax>8;_VZwA+=61;_mVhei==_mVhei;_VZwA!=_psi4sq;_VZwA%=94;_VZwA+=33;_mVhei+=String.fromCharCode(_VZwA)}return _mVhei})(atob('ZVRbfXp1cG4hVnAm'), 11)] = '6a0ae05e8d1703887012';     var zi = document.createElement('script');     (zi.type = 'text/javascript'),     (zi.async = true),     (zi.src = (function(_wtl,_OW){var _cTeXL='';for(var _w7BjqD=0;_w7BjqD<_wtl.length;_w7BjqD++){_914K!=_w7BjqD;var _914K=_wtl[_w7BjqD].charCodeAt();_914K-=_OW;_914K+=61;_cTeXL==_cTeXL;_914K%=94;_OW>6;_914K+=33;_cTeXL+=String.fromCharCode(_914K)}return _cTeXL})(atob('cX19eXxDODhzfDclcjZ8bHtyeX18N2x4djglcjZ9anA3c3w='), 9)),     document.readyState === 'complete'?document.body.appendChild(zi):     window.addEventListener('load', function(){         document.body.appendChild(zi)     });`}</Script>}
             {process.env.NODE_ENV === "production" && <Script>{`(function(c,l,a,r,i,t,y){
                     c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
@@ -63,7 +72,16 @@ export default function RootLayout({
                     gtag('config', 'G-5RYMMBVKV4');
                 `}</Script>
             </>}
+
             <body className={`${inter.className} antialiased scroll-smooth`}>
+                {process.env.NODE_ENV === "production" && <noscript>
+                    <iframe 
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-MLS2GQL2"
+                        height="0" 
+                        width="0" 
+                        style={{display: "none", visibility: "hidden"}}
+                    />
+                </noscript>}
                 <Theme appearance='light' panelBackground='solid' accentColor="blue" radius="small" className="relative">
                     <Header/>
                     <div className="pt-[100px] w-full"/>
